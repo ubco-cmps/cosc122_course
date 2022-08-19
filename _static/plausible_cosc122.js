723cda456171530e1e065345b639bf01eaf163b2
Firas Moosvi <firas@moosvi.com>
2022-08-19 10:27:50 -0700

    update PA

--- _static/plausible_cosc122.js
+++ _static/plausible_cosc122.js
@@ -1,7 +1,7 @@
 var script = document.createElement("script");
 script.defer = true;
 script.src = "https://plausible.io/js/script.js";
-script.dataset.domain = "firas.moosvi.com/courses/cosc122";
+script.dataset.domain = "cosc122";
 
 // optional if using proxy
 // script.dataset.api = 'https://yourproxy.com/api/event';
